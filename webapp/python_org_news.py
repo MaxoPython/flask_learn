b353469cbb817de2b830ee473118be138a33bb56
Maxim Odintsov <maxim.odintsov@inwin.tech>
2019-03-25 23:12:28 +0300

    First commit

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
+
+from webapp.model import db, News
 
 def get_html(url):
     try:
@@ -17,15 +21,19 @@ def get_python_news():
         all_news=soup.find('ul',class_='list-recent-posts').findAll('li')
         result_news=[]
         for news in all_news:
-            title1=news.find('a').text
+            title=news.find('a').text
             url=news.find('a')['href']
             published=news.find('time').text
-            result_news.append({
-                'title':title1,
-                'url':url,
-                'published':published
+            try:
+                published = datetime.strptime(published, '%y-%m-%d')
+            except ValueError:
+                published = datetime.now()
+            save_news(title, url, published)
 
-            })
-        return result_news
-    return False
-
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        news_news = News(title=title, url=url, published=published)
+        db.session.add(news_news)
+        db.session.commit()
